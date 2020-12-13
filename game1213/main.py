9491680797976e4a66c29c2e26f8fa537100b9f6
zino1187 <zino1187@gmail.com>
2020-12-13 10:17:51 +0900

    c

--- game1213/main.py
+++ game1213/main.py
@@ -3,11 +3,17 @@ from tkinter import Tk #tkinter라는 파일에 들어있는 Tk 함수를 쓰겠
 from tkinter import Canvas #tkinter라는 파일에 들어있는 Canvas 함수를 쓰겠다
 import time #time이라는 모듈파일을 쓰겠다!!
 from PIL import Image
+from PIL import ImageTk 
 
 # 이미지 생성 함수 : 이 함수를 호출하는 자는, 이미지를 얻어갈수 있도록...
 def getImage():
-    Image.open("") #Image 객체는 pillow 모듈파일에서 지원한다..따라서 현재 개발환경에
-                        #설치가 되어 있어야 한다..
+    img=Image.open("./images/desert.jpg")  #Image 객체는 pillow 모듈파일에서 지원한다..따라서 현재 개발환경에
+                            #설치가 되어 있어야 한다..
+    #크기를 우리가 원하느 크기로 재조정하자!!!
+    img=img.resize((1500,800), Image.ANTIALIAS) #재조정된 이미지를 다시변수로받기
+    #크기가 재조정된 이미지를 켄버스에서 사용할 수 있도록 가공 
+    img = ImageTk.PhotoImage(img) #img를 켄버스가 이해할수있는 이미지로 변환
+    return img #함수 호출한 사람에게 결과를 반환하자!!
 
 win=Tk() #윈도우 생성하고 win 변수에 담아둔다
 #윈도우에 소속시킬 켄버스를 생성하고, 그 크기를 너비1500,높이 800
