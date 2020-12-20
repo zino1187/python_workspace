ce7502f6db908f797cc9748b263a1376def39185
zino1187 <zino1187@gmail.com>
2020-12-20 09:22:53 +0900

    c

--- game1213/게임완성.py
+++ game1213/게임완성.py
@@ -1,4 +1,5 @@
 from tkinter import Tk  #tkinter라는 파일에서 Tk() 함수가져오기
+from tkinter import Canvas #tkinter라는 파일에서 Canvas가져오기
 
 #지난 시간에 거푸집을 프로그래밍 분야에서는 클래스라 한다.
 class GameMain():
@@ -6,6 +7,9 @@ class GameMain():
                                 #생성자는 거푸집으로부터 물건을 생성해 낼때 어떤 모양, 색상, 특징을 
                                 #갖는지를 결정짓는 함수다..    
         self.win=Tk() #윈도우창 호출하고, 그 윈도우를 가리킬 변수 선언
+        self.canvas = Canvas(self.win, width=1600, height=900) #켄버스 생성
+        self.canvas.pack() #윈도우에 부착하기!!
+
         self.win.mainloop() #윈도우가 사라지지않고 계속 유지될 수 있게 루프실행
 
 GameMain() #거푸집으로부터, 물체 하나를 생성한다.. 
